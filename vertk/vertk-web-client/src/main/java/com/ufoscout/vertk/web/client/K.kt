7e0398f0a5ebcd30a1f8f7287c8e8e6d05f9454d
ufoscout <ufoscout@gmail.com>
2019-07-02 10:01:44 +0200

    Add kotlin jackoson by default

--- vertk/vertk-web-client/src/main/java/com/ufoscout/vertk/web/client/K.kt
+++ vertk/vertk-web-client/src/main/java/com/ufoscout/vertk/web/client/K.kt
@@ -1,13 +1,15 @@
 package com.ufoscout.vertk.web.client
 
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.vertx.core.json.Json
 import io.vertx.ext.web.client.HttpRequest
 import io.vertx.ext.web.client.HttpResponse
 
-fun <T : Any> HttpRequest<T>.putHeaders(vararg headers: Pair<String, String>): HttpRequest<T> {
+fun <T> HttpRequest<T>.putHeaders(vararg headers: Pair<String, String>): HttpRequest<T> {
     headers.forEach { this.putHeader(it.first, it.second) }
     return this
 }
 
-inline fun <reified T : Any> HttpResponse<out Any>.bodyAsJson(): T {
-        return this.bodyAsJson(T::class.java)
+inline fun <reified T> HttpResponse<out Any>.bodyAsJson(): T {
+    return Json.mapper.readValue(this.bodyAsString())
 }
